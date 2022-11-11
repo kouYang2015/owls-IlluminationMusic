de5c6141a01a5e35550e87ae4ab3f6a2290a6e79
Kou <kou.yang2015@gmail.com>
2022-11-11 16:55:14 -0600

    Updated logout page and created logout.css file

--- src/logout.php
+++ src/logout.php
@@ -4,6 +4,7 @@
 <head>
     <title>Illumination Music - Logout</title>
     <link rel="stylesheet" type="text/css" href="css/theme.css" />
+    <link rel="stylesheet" type="text/css" href="css/logout.css" />
 </head>
 <header>
     <div class="navbar">
@@ -22,8 +23,12 @@
 </header>
 
 <body>
-    <h1 align="center">Logout</h1>
-    <p>In progress</p>
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/512px-Eo_circle_green_checkmark.svg.png?20200417132424" alt="check" style="width: 150px; height: 150px">
+    </div>
+    <h1>You have been successfully logged out!</h1>
+    <p>Thank you!</p>
+    <div><a href="homePage.html">Go back to homepage</a></div>
 </body>
 
 </html>
