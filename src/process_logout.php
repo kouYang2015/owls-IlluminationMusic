d608c44ce82671c0c8e1eef133957a10adfb8cf3
Mariko16 <86332600+Mariko16@users.noreply.github.com>
2022-11-10 20:39:25 -0600

    Created logout page

---
+++ src/process_logout.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Illumination Music - Logout Results</title>
+    <style>
+        p {
+            text-align: center;
+            font-size: 20px;
+        }
+
+        .back {
+            text-align: center;
+            font-size: 20px;
+        }
+
+        .image {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="image">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/512px-Eo_circle_green_checkmark.svg.png?20200417132424" alt="check" style="width: 150px; height: 150px">
+    </div>
+    <h1 align="center">You have been successfully logged out!</h1>
+    <p>Thank you!</p>
+    <div class="back"><a href="homePage.html">Go back to homepage</a></div>
+
+</body>
+
+</html>
