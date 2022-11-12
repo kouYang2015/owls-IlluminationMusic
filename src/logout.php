a468a2725884031aa61b41db3bc59534ab9d99cd
Mariko16 <86332600+Mariko16@users.noreply.github.com>
2022-11-12 17:35:52 -0600

    User setting (#7)
    * Created user_setting page
    
    * FIle can save the images but cannot display it yet
    
    * Made a temporary logout page to navigate to
    
    * Allow user to upload image on user_settings
    
    * Added todo comments for after integrating database
    
    * Updated logout page and created logout.css file
    
    * refractor upload.php and included it in
    user_settings.php

---
+++ src/logout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Illumination Music - Logout</title>
+    <link rel="stylesheet" type="text/css" href="css/theme.css" />
+    <link rel="stylesheet" type="text/css" href="css/logout.css" />
+</head>
+<header>
+    <div class="navbar">
+        <a href="homePage.html"><img src="https://brandeps.com/icon-download/M/Music-icon-vector-03.svg" style="width: 50px; height: 50px" /></a>
+        <a href="homePage.html">Home</a>
+        <a href="userpage.php">Playlist</a>
+        <div class="dropdown">
+            <img class="dropbtn" src="https://pic.onlinewebfonts.com/svg/img_24787.png" style="width: 40px; height: 40px" />
+            <div class="dropdown-content">
+                <a href="user_setting.php">Account</a>
+                <a href="EditProfile_Email.html">Security</a>
+                <a href="logout.php">Log out</a>
+            </div>
+        </div>
+    </div>
+</header>
+
+<body>
+    <div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Eo_circle_green_checkmark.svg/512px-Eo_circle_green_checkmark.svg.png?20200417132424" alt="check" style="width: 150px; height: 150px">
+    </div>
+    <h1>You have been successfully logged out!</h1>
+    <p>Thank you!</p>
+    <div><a href="homePage.html">Go back to homepage</a></div>
+</body>
+
+</html>
--- src/user_setting.php
+++ src/user_setting.php
@@ -1,10 +1,75 @@
 <!DOCTYPE html>
 <html>
-  <head>
-   <title>Illumination Music - User settings</title>
-  </head>
-  <body>
-    <h1 align="center">User Settings</h1>
-    <p>Page under construction. Look at mockup for template.</p>
-  </body>
+
+<head>
+  <title>Illumination Music - User settings</title>
+  <link rel="stylesheet" type="text/css" href="css/theme.css" />
+  <link rel="stylesheet" type="text/css" href="css/user_setting.css" />
+</head>
+<header>
+  <div class="navbar">
+    <a href="homePage.html"><img src="https://brandeps.com/icon-download/M/Music-icon-vector-03.svg" style="width: 50px; height: 50px" /></a>
+    <a href="homePage.html">Home</a>
+    <a href="userpage.php">Playlist</a>
+    <div class="dropdown">
+      <img class="dropbtn" src="https://pic.onlinewebfonts.com/svg/img_24787.png" style="width: 40px; height: 40px" />
+      <div class="dropdown-content">
+        <a href="user_setting.php">Account</a>
+        <a href="EditProfile_Email.html">Security</a>
+        <a href="logout.php">Log out</a>
+      </div>
+    </div>
+  </div>
+</header>
+
+<body>
+
+  <h1>Account Overview</h1>
+  <div class="profile_icon">
+    <?php
+    if (isset($_POST['submit']) && is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
+      include 'upload.php';
+    } else {
+      if (false) { // TODO: update if statement to check database if there is a filpath for user.
+        //TODO: load current profile image path from database
+      } else {
+        echo '<img src="https://brandeps.com/icon-download/M/Music-icon-vector-03.svg" width=180 height=180">';
+      }
+    }
+    ?>
+  </div>
+  <div class="upload">
+    <form action="user_setting.php" method="post" enctype="multipart/form-data">
+      Select a profile image to upload: </p>
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Upload Image" name="submit">
+    </form>
+  </div>
+  <br>
+
+  <?php
+  $userInfo = array(array(
+    'username' => 'name',
+    'Email' => '1234abcde@gmail.com'
+  ));
+  // TODO: after database integration, actually use the username and email from database.
+
+  foreach ($userInfo as $info) {
+    echo '<i><p style="font-family:arial; font-size:20px; text-align:center">' . "Username: " . $info['username'] . "</p>";
+    echo '<i><p style="font-family:arial; font-size:20px; text-align:center">' . "Email: " . $info['Email'] . "</p>";
+  }
+  ?>
+
+  <br>
+  <div class="editbtn" style="text-align: center">
+    <a href="EditProfile_Email.html"><button type="submit" class="edit" id="edit" value="Edit Profile">Edit Profile</button></a>
+  </div>
+
+  <br><br>
+  <div class="delete" style="text-align: center">
+    <button type="submit" class="deletebtn" id="delete" value="delete Account">Delete Account</button>
+  </div>
+
+</body>
+
 </html>
