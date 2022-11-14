184a06c1c50d9d617a245f0c240b386aabaa3d6e
Kou <kou.yang2015@gmail.com>
2022-11-14 01:58:02 -0600

    signup page now adds new user to local db

--- src/process_signup.php
+++ src/process_signup.php
@@ -6,6 +6,7 @@ $email = $_POST['email'];
 $email_confirm = $_POST['email_confirm'];
 $password = $_POST['password'];
 $password_confirm = $_POST['password_confirm'];
+include 'database.php';
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,13 +20,7 @@ $password_confirm = $_POST['password_confirm'];
   <div align="center">
     <h3>Your Entries:</h3>
     <?php
-    echo 'Username: ' . $uname . '<br>';
-    echo 'First name: ' . $fname . '<br>';
-    echo 'Last name: ' . $lname . '<br>';
-    echo 'Email: ' . $email . '<br>';
-    echo 'Email Confirmation: ' . $email_confirm . '<br>';
-    echo 'Password: ' . $password . '<br>';
-    echo 'Password Confirmation: ' . $password_confirm . '<br>';
+    insertNewUser($uname, $fname, $lname, $email, $password);
     ?>
     <br>
     <h3> Under Construction. Next step: Validate username or email is not in database before adding to database. </h3>
