aea6c8c93af3de489a54d0ff0ee000a893c48e43
Kou <kou.yang2015@gmail.com>
2022-11-14 00:51:39 -0600

    setup local database connection

--- src/database.php
+++ src/database.php
@@ -1,7 +1,12 @@
 <?php
 //Connect to database
-@$db = mysqli_connect("localhost", "ics325fa2226", "9427", "ics325fa2226");
-echo $mysqli->host_info . "\n";
+//@$db = mysqli_connect("localhost", "ics325fa2226", "9427", "ics325fa2226"); // Use when using metrostate server
+@$db = mysqli_connect("localhost", "root", "", "illumination_local"); //Use when working offline and locally
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+} else {
+    echo "Connection success!";
+}
 
 //create all our read, insert, update, and delete query statements
 
