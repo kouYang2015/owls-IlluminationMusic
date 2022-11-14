b920b3b1bbdafd226f1cd47681ff8835056df0fc
Kou <kou.yang2015@gmail.com>
2022-11-14 00:36:13 -0600

    connect to database.

---
+++ src/database.php
@@ -0,0 +1,16 @@
+<?php
+//Connect to database
+@$db = mysqli_connect("localhost", "ics325fa2226", "9427", "ics325fa2226");
+echo $mysqli->host_info . "\n";
+
+//create all our read, insert, update, and delete query statements
+
+function updateUserEmail($username, $newEmail)
+{
+    echo '';
+}
+
+function updateUserPassword($username, $newPassword)
+{
+    echo '';
+}
