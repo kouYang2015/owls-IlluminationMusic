7ebbeb5b37f46c986378ebb53edd60f0dae2a81d
Kou <kou.yang2015@gmail.com>
2022-11-14 01:37:24 -0600

    added function to add new user to database

--- src/database.php
+++ src/database.php
@@ -1,21 +1,43 @@
 <?php
 //Connect to database
 //@$db = mysqli_connect("localhost", "ics325fa2226", "9427", "ics325fa2226"); // Use when using metrostate server
-@$db = mysqli_connect("localhost", "root", "", "illumination_local"); //Use when working offline and locally
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} else {
-    echo "Connection success!";
-}
 
 //create all our read, insert, update, and delete query statements
 
-function updateUserEmail($username, $newEmail)
+function insertNewUser($username, $first_name, $last_name, $newEmail, $user_password)
+{
+    @$db = mysqli_connect("localhost", "root", "", "illumination_local"); //Use when working offline and locally
+    if (mysqli_connect_errno()) {
+        echo "<p>Error: Could not connect to database.<br/>
+          Please try again later.</p>";
+        exit;
+    }
+    $query = "INSERT INTO users(username, first_name, last_name, user_email, user_password)
+    VALUES(?, ?, ?, ?, ?)";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('sssss', $username, $first_name, $last_name, $newEmail, $user_password);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        echo 'Username: ' . $username . '<br>';
+        echo 'First name: ' . $first_name . '<br>';
+        echo 'Last name: ' . $last_name . '<br>';
+        echo 'Email: ' . $newEmail . '<br>';
+        echo 'Password: ' . $user_password . '<br>';
+    } else {
+        echo "<p>An error has occurred.<br/>
+           Could not sign up.</p>";
+    }
+
+    $db->close();
+}
+
+function updateUserPassword($username, $newPassword)
 {
     echo '';
 }
 
-function updateUserPassword($username, $newPassword)
+function updateAdminPassword($newPassword)
 {
     echo '';
 }
