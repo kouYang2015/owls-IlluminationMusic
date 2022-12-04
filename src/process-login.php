1061f221877f5333b44d9c549b62d1fb6915f928
Kou <kou.yang2015@gmail.com>
2022-12-04 02:55:09 -0600

    Functionality to login user. Redirects to login if unable to login.

--- src/process-login.php
+++ src/process-login.php
@@ -9,13 +9,36 @@
 
 <body>
   <?php include_once 'session.php';
-  setSession();
-  loginUser("fakeUser");
+  include_once 'database.php';
+  if (isset($_POST['login_user']) && isset($_POST['login_password'])) {
+    $login_user = htmlspecialchars($_POST['login_user']);
+    $login_password = htmlspecialchars($_POST['login_password']);
+    if (validateUsernameEmail($login_user)) {
+      echo'validated user';
+      if (filter_var($login_user, FILTER_VALIDATE_EMAIL) && validateEmailPassword($login_user, $login_password)) {
+        setSession();
+        loginUser($login_user);
+      } 
+      elseif (validateUsernamePassword($login_user, $login_password)) {
+        setSession();
+        loginUser($login_user);
+      } 
+      else {
+        header('Location: login.php?login_user='.$login_user.'&errcode=1');
+        exit;
+      }
+    }
+    else {
+      header('Location: login.php?login_user='.$login_user.'&errcode=2');
+      exit;
+    }
+  } else {
+    header('Location: login.php?login_user='.$login_user.'&errcode=3');
+    exit;
+  }
   include_once 'header.php';
   ?>
-  <h1 align="center">Login Results</h1>
-  <p>Page under construction. Look at mockup for template.</p>
-
+  <h1 align="center">Login</h1>
 </body>
 
 </html>
