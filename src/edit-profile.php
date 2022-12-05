be1b1e61cc212218c39e7b196c1cb2d8bd4997ab
Kou <kou.yang2015@gmail.com>
2022-12-05 03:19:00 -0600

    updating email or password is now validated.

--- src/edit-profile.php
+++ src/edit-profile.php
@@ -10,9 +10,25 @@
 <body>
     <?php include 'header.php'; 
     include 'database.php'?>
-
-    <h1><u>Change Email Address</u></h1>
-
+    <h1><u>Edit Profile</u></h1>
+    <h2><u>Change Email Address</u></h2>
+    <?php 
+    if (isset($_GET['errcode'])) {
+        switch($_GET['errcode']) {
+        case(1):
+            echo '<h3 class="errorText">Email & email confirmation does not match!</h3>';
+            break;
+        case(2):
+            echo '<h3 class="errorText">Not valid email input!</h3>';
+            break;
+        case(3):
+            echo '<h3 class="errorText">One or more fields empty!</h3>';
+            break;
+        default:
+            break;
+        }
+    }
+    ?>
     <form action="process-edit-profile.php" method="post">
             <table>
                 <tr>
@@ -31,12 +47,33 @@
                 </tr>
             </table>
         <div style="text-align:center">
-            <button type="submit" name="updateEmail" value="Request email update" class="submitbtn">Update Email</button>
+            <button type="submit" name="updateEmail" value="updateEmail" class="submitbtn">Update Email</button>
         </div>
     </form>
 
+    <h2><u>Change Password</u></h2>
+    <?php 
+        if (isset($_GET['errcode'])) {
+            switch($_GET['errcode']) {
+            case(4):
+                echo '<h3 class="errorText">New password & password confirmation does not match!</h3>';
+                break;
+            case(5):
+                echo '<h3 class="errorText">New password must be minimum eight characters and include at least one uppercase letter, 
+                one lowercase letter, one number and one special character (@$!%*?&)</h3>';
+                break;
+            case(6):
+                echo '<h3 class="errorText">Password does not match current password!</h3>';
+                break;
+            case(7):
+                echo '<h3 class="errorText">One or more fields empty!</h3>';
+                break;
+            default:
+                break;
+            }
+        }
+    ?>
     <form action="process-edit-profile.php" method="post">
-        <h2><u>Change Password</u></h2>
         <div>
             <table>
                 <tr>
@@ -54,7 +91,7 @@
             </table>
         </div>
         <div style="text-align:center">
-            <button type="submit" name="updatePw" value="Request password update" class="submitbtn">Update Password</button>
+            <button type="submit" name="updatePw" value="updatePw" class="submitbtn">Update Password</button>
         </div>
     </form>
 
--- src/process-edit-profile.php
+++ src/process-edit-profile.php
@@ -1,16 +1,4 @@
 <!DOCTYPE html>
-<?php
-if (isset($_POST['updateEmail'])) {
-  $newEmail = htmlspecialchars($_POST['new_address']);
-  $email_confirm = htmlspecialchars($_POST['confirm_address']);
-}
-if (isset($_POST['updatePw'])) {
-  $currentPassword = htmlspecialchars($_POST['current_password']);
-  $newPassword = htmlspecialchars($_POST['new_password']);
-  $newPassword_confirm = htmlspecialchars($_POST['confirm_password']);
-}
-include 'database.php';
-?>
 <html>
 
 <head>
@@ -24,30 +12,94 @@ include 'database.php';
   <h1 align="center">Edit Profile</h1>
   <div align="center">
     <?php
-    $usernameToSearchFor = 'johndoe123';
-    if (isset($_POST['updateEmail'])) {
-      if ($newEmail == $email_confirm) {
-        updateEmail($usernameToSearchFor, $newEmail);
-      } else {
-        echo 'Email & email confirmation does not match! <br>';
-        echo '<a href="edit-profile.php"><button type="submit" class="buttonContainer">Try Again</button></a>';
-      }
-    }
-    if (isset($_POST['updatePw'])) {
-      $usernameToSearchFor = 'johndoe123';
-      if (validateUsernamePassword($usernameToSearchFor, $currentPassword)) {
-        if ($newPassword == $newPassword_confirm) {
-          updatePassword($usernameToSearchFor, $newPassword);
+    include 'database.php';
+    $usernameToSearchFor = $_SESSION['username'];
+    if (isset($_POST['updateEmail']) && array_key_exists('updateEmail', $_POST)){
+      if (emailFieldsExists() ) {
+        $newEmail = htmlspecialchars($_POST['new_address']);
+        $email_confirm = htmlspecialchars($_POST['confirm_address']);
+        if (filter_var($newEmail, FILTER_VALIDATE_EMAIL) && filter_var($email_confirm, FILTER_VALIDATE_EMAIL)) {
+          if ($newEmail == $email_confirm) {
+            updateEmail($usernameToSearchFor, $newEmail);
+            echo '<h2>Email updated!</h2>';
+          } else {
+            //Email & email confirmation does not match.
+            header('Location: edit-profile.php?errcode=1');
+            exit;
+          }
         } else {
-          echo 'New password & password confirmation does not match!';
-          echo '<a href="edit-profile.php"><button type="submit" class="buttonContainer">Try Again</button></a>';
+          //Not valid email input
+          header('Location: edit-profile.php?errcode=2');
+          exit;
         }
       } else {
-        echo 'Password does not match current password!';
-        echo '<a href="edit-profile.php"><button type="submit" class="buttonContainer">Try Again</button></a>';
+        //Empty input fields
+        header('Location: edit-profile.php?errcode=3');
+        exit;
+      }
+    } elseif (isset($_POST['updatePw']) && array_key_exists('updatePw', $_POST)) {
+      if (pwFieldsExists()) {
+        $currentPassword = $_POST['current_password'];
+        $newPassword = $_POST['new_password'];
+        $newPassword_confirm = $_POST['confirm_password'];
+
+        if (validateUsernamePassword($usernameToSearchFor, $currentPassword)) {
+          if (pwPassRegex($newPassword) && pwPassRegex($newPassword_confirm)) {
+            if ($newPassword == $newPassword_confirm) {
+              updatePassword($usernameToSearchFor, $newPassword);
+              echo '<h2>Password updated!</h2>';
+            } else {
+              //New password & password confirmation does not match
+              header('Location: edit-profile.php?errcode=4');
+              exit;
+            }
+          } else {
+            //New password does not match regex pattern
+            header('Location: edit-profile.php?errcode=5');
+            exit;
+          } 
+        } else {
+        //Password does not match current password
+        header('Location: edit-profile.php?errcode=6');
+        exit;
+        }
+      } else {
+        //Empty input fields
+        header('Location: edit-profile.php?errcode=7');
+        exit;
       }
     }
+
+
+
+    function emailFieldsExists() {
+      if (isset($_POST['new_address']) && array_key_exists('new_address', $_POST) &&  $_POST['new_address'] != null &&
+      isset($_POST['confirm_address']) && array_key_exists('confirm_address', $_POST) && $_POST['confirm_address'] != null) {
+        return true;
+      }
+      return false;
+    }
+
+    function pwFieldsExists() {
+      if (isset($_POST['current_password']) && array_key_exists('current_password', $_POST) && $_POST['current_password'] != null &&
+      isset($_POST['new_password']) && array_key_exists('new_password', $_POST) && $_POST['new_password'] != null &&
+      isset($_POST['confirm_password']) && array_key_exists('confirm_password', $_POST) && $_POST['confirm_password'] != null) {
+        return true;
+      }
+      return false;
+    }
+
+    function pwPassRegex($password){
+      $regex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/";
+      if (preg_match($regex, $password)){
+        return true;
+      }
+      return false;
+    }
     ?>
+  </div>
+  <div class="container">
+    <img src="images/green_checkmark.png" alt="check">
   </div>
 </body>
 
