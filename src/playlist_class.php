bcabcf03f67392b35366c74f17afa960ce3693b1
Kou <kou.yang2015@gmail.com>
2022-12-05 16:17:30 -0600

    created class for playlist

---
+++ src/playlist_class.php
@@ -0,0 +1,29 @@
+<?php
+  class playlist
+  {
+    private $playlist_ids;
+    private $playlist_names;
+    private $playlist_imgfilename;
+
+    function __construct($playlistID, $playlistName, $imgName)
+    {
+      $this->playlistID = $playlistID;
+      $this->playloistName = $playlistName;
+      $this->imgName = $imgName;
+    }
+
+    public function playlistID()
+    {
+      return $this->playlistID;
+    }
+    public function playlistName()
+    {
+      return $this->playlistName;
+    }
+    public function imgName()
+    {
+      return $this->imgName;
+    }
+  }
+
+  ?>
