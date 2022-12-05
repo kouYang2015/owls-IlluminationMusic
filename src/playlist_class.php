ac45e290c06654ea113604204217f7634ed88d98
Kou <kou.yang2015@gmail.com>
2022-12-05 16:19:16 -0600

    fixed constructor and getter functions

--- src/playlist_class.php
+++ src/playlist_class.php
@@ -1,28 +1,28 @@
 <?php
   class Playlist
   {
-    private $playlist_ids;
-    private $playlist_names;
+    private $playlist_id;
+    private $playlist_name;
     private $playlist_imgfilename;
 
     function __construct($playlistID, $playlistName, $imgName)
     {
-      $this->playlistID = $playlistID;
-      $this->playloistName = $playlistName;
-      $this->imgName = $imgName;
+      $this->playlist_id = $playlistID;
+      $this->playlist_name = $playlistName;
+      $this->playlist_imgfilename = $imgName;
     }
 
-    public function playlistID()
+    public function getPlaylistID()
     {
-      return $this->playlistID;
+      return $this->playlist_id;
     }
-    public function playlistName()
+    public function getPlaylistName()
     {
-      return $this->playlistName;
+      return $this->playlist_name;
     }
-    public function imgName()
+    public function getImgFileName()
     {
-      return $this->imgName;
+      return $this->playlist_imgfilename;
     }
   }
 
