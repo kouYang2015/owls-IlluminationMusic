9b1eb3b1cb04e0343b7f7643a4dae1918a055dbb
Mariko16 <86332600+Mariko16@users.noreply.github.com>
2022-12-05 21:12:50 -0600

    Added more css for the sign-up page (#24)
    * Added more css for the sign-up page
    
    * moved signup specific css to unique css file

--- src/signup.php
+++ src/signup.php
@@ -4,6 +4,7 @@
 <head>
   <title>Illumination Music - Signup</title>
   <link rel="stylesheet" type="text/css" href="css/theme.css" />
+  <link rel="stylesheet" type="text/css" href="css/signup.css" />
 </head>
 
 <body>
