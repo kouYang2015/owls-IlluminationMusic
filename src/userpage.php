4cff1feff03ef67c0446019053292a2bec18392a
Kou Yang <80478758+kouYang2015@users.noreply.github.com>
2022-12-05 21:30:20 -0600

    Update CSS for user_setting, userpage, header (#26)
    * header css fixed
    
    * User playlist page css fixed. Aligned contents
    
    * user setting css fixed. contents aligned.
    
    * fixed header css

--- src/userpage.php
+++ src/userpage.php
@@ -5,54 +5,35 @@
   <title>Illumination Music - Edit Profile</title>
   <link rel="stylesheet" type="text/css" href="css/theme.css" />
   <link rel="stylesheet" type="text/css" href="css/userpage.css" />
-  <style>
-    td {
-      display: inline-block;
-    }
-
-    .subtitle {
-      text-align: center;
-    }
-
-    .grid-item {
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-      padding-top: 5%;
-    }
-  </style>
 </head>
 
 <body>
   <?php include 'header.php'; ?>
   <?php include 'database.php'; ?>
 
-  <div class="subtitle">
+  <div class="search-container">
     <h1>Your Playlists</h1>
+    <form action="" class="searchPlaylist">
+      <input type="text" placeholder="Search.." id="search" name="search">
+      <button type="submit"><i class="fa fa-search"></i>search</button>
+    </form>
   </div>
-  <form action="search-action-page" class="searchPlaylist">
-    <input type="text" placeholder="Search.." id="search" name="search" style="width:200px;height:30px;">
-    <button type="submit"><i class="fa fa-search"></i>search</button>
-    <br><br>
-  </form>
-
+  <div class="grid-container">
   <?php
   $playlistarray = retrievePlaylists($_SESSION['username']);
-  echo '<div class="grid-item" id="image">';
   foreach ($playlistarray as $playlist) {
     echo
-    '<form action="playlist.php" method="post">
-      <button style = "padding10px;" type="submit" name="playlist_id" value="' . $playlist->getPlaylistID() . '">
-      <table>
-        <tr><td style="font-size:23px; font-weight:bold; float :">' . $playlist->getPlaylistName() . '</td></tr>
-        <center><tr><td><img style="height:150px; width: 150px; padding: 10px; float: left" src="' . $playlist->getImgFileName() . '"></td></tr></center>
-      </table>
+    '<div class="grid-item">
+    <form action="playlist.php" method="post">
+      <button type="submit" name="playlist_id" value="' . $playlist->getPlaylistID() . '">
+      <p>' . $playlist->getPlaylistName() . '<p>
+      <img src="' . $playlist->getImgFileName() . '">
       </button>
-    </form>';
+    </form>
+    </div>';
   }
-
-  echo '</div>';
   ?>
-
+  </div>
 </body>
 
 </html>
