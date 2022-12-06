10db596609d771a39e205a2b7198410ef4777062
Kou Yang <80478758+kouYang2015@users.noreply.github.com>
2022-12-06 00:09:05 -0600

    Tech css colors (#30)
    * removed unused files
    
    * added linear gradien colors
    
    * add rationale if playlists are empty
    
    * add cursor point to all buttons and inputs buttons

--- src/playlist.php
+++ src/playlist.php
@@ -6,64 +6,67 @@
   <link rel="stylesheet" type="text/css" href="css/theme.css" />
   <link rel="stylesheet" type="text/css" href="css/process-generate-playlist.css" />
 </head>
-  <body>
-    <?php include "header.php"; 
-    include 'database.php';?>
-    <div class="info-container">
-      <h1 contenteditable="true">
-        <?php 
-            if (isset($_SESSION['username'])){
-              if (isset($_POST['playlist_id'])){
-                $playlist = retrievePlaylistObject($_POST['playlist_id']);
-                echo $playlist->getPlaylistName();
-              } else {
-                echo 'New Playlist';
-              }
-            }
-        ?>
-      </h1>
-      <img src="
-      <?php 
-            if (isset($_SESSION['username'])){
-              if (isset($_POST['playlist_id'])){
-                $playlist = retrievePlaylistObject($_POST['playlist_id']);
-                echo $playlist->getImgFileName();
-              } else {
-                echo 'uploads/Illumination.png';
-              }
-            }
-        ?>
-        " style="width: 250px; height: 250px" />
-      <div>
-        <a href="generate-playlist.php"><button type="button" value="gen">Generate New Playlist</button></a>
-      </div>
-    </div>
 
-    <table id="playlistTable">
-      <tr>
-        <th>Title</th>
-        <th>Artist</th>
-        <th>Album</th>
-      </tr>
+<body>
+  <?php include "header.php";
+  include 'database.php'; ?>
+  <div class="info-container">
+    <h1 contenteditable="true">
       <?php
-      if (isset($_SESSION['username'])){
-        if (isset($_SESSION['temp_playlist'])){
-          $new_song_list = unserialize($_SESSION['temp_playlist']);
-          foreach($new_song_list as $song) {
-            echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() .'</td></tr>';
-          }
-        } elseif (isset($_POST['playlist_id'])){
-          $new_song_list = retreivePlaylist($_POST['playlist_id']);
-          foreach($new_song_list as $song) {
-            echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() .'</td></tr>';
-          }
+      if (isset($_SESSION['username'])) {
+        if (isset($_POST['playlist_id'])) {
+          $playlist = retrievePlaylistObject($_POST['playlist_id']);
+          echo $playlist->getPlaylistName();
+        } else {
+          echo 'New Playlist';
         }
       }
       ?>
-    </table>
-  </body>
+    </h1>
+    <img src="
+      <?php
+      if (isset($_SESSION['username'])) {
+        if (isset($_POST['playlist_id'])) {
+          $playlist = retrievePlaylistObject($_POST['playlist_id']);
+          echo $playlist->getImgFileName();
+        } else {
+          echo 'uploads/Illumination.png';
+        }
+      }
+      ?>
+        " style="width: 250px; height: 250px" />
+    <div>
+      <a href="generate-playlist.php"><button type="button" value="gen">Generate New Playlist</button></a>
+    </div>
+  </div>
+
+  <table id="playlistTable">
+    <tr>
+      <th>Title</th>
+      <th>Artist</th>
+      <th>Album</th>
+    </tr>
+    <?php
+    if (isset($_SESSION['username'])) {
+      if (isset($_SESSION['temp_playlist'])) {
+        $new_song_list = unserialize($_SESSION['temp_playlist']);
+        foreach ($new_song_list as $song) {
+          echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() . '</td></tr>';
+        }
+      } elseif (isset($_POST['playlist_id'])) {
+        $new_song_list = retreivePlaylist($_POST['playlist_id']);
+        foreach ($new_song_list as $song) {
+          echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() . '</td></tr>';
+        }
+      }
+    } else {
+      "<h2>Playlist Empty</h2>";
+    }
+    ?>
+  </table>
 
   <script src="yt-link-script.js"></script>
 
-</html>
+</body>
 
+</html>
--- src/process-generate-playlist.php
+++ src/process-generate-playlist.php
@@ -6,87 +6,92 @@
   <link rel="stylesheet" type="text/css" href="css/theme.css" />
   <link rel="stylesheet" type="text/css" href="css/process-generate-playlist.css" />
 </head>
-  <body>
-    <?php include "header.php"; ?>
-    <div class="info-container">
-      <h1 contenteditable="true">New Playlist</h1>
-      <img src="images/illumination_logo.png" style="width: 250px; height: 250px" />
-      
-      <div>
-        <a href="generate-playlist.php"><button type="button" value="gen">Generate New Playlist</button></a>
-      </div>
-    </div>
 
-    <table id="playlistTable">
+<body>
+  <?php include "header.php"; ?>
+  <div class="info-container">
+    <h1 contenteditable="true">New Playlist</h1>
+    <img src="images/illumination_logo.png" style="width: 250px; height: 250px" />
+
+    <div>
+      <a href="generate-playlist.php"><button type="button" value="gen">Generate New Playlist</button></a>
+    </div>
+  </div>
+
+  <table id="playlistTable">
+    <?php
+    include 'database.php';
+    $new_song_list = array();
+    if (isset($_POST['genre']) && array_key_exists('genre', $_POST)) {
+      $genre_list = $_POST['genre'];
+      foreach ($genre_list as $genre) {
+        if ($genre != 'select') {
+          $new_song_list = array_merge($new_song_list, retrieveSongsGenre($genre));
+        }
+      }
+    }
+    if (isset($_POST['year-max']) && array_key_exists('year-max', $_POST) && isset($_POST['year-min']) && array_key_exists('year-min', $_POST)) {
+      $year_max = $_POST['year-max'];
+      $year_min = $_POST['year-min'];
+      if ($year_min != "" && $year_max != "") {
+        $new_song_list = array_merge($new_song_list, retrieveSongsYear($year_max, $year_min));
+      }
+    }
+    if (isset($_POST['album']) && array_key_exists('album', $_POST)) {
+      $album_list = $_POST['album'];
+      foreach ($album_list as $album) {
+        if ($album != 'select') {
+          $new_song_list = array_merge($new_song_list, retrieveSongsAlbum($album));
+        }
+      }
+    }
+    if (isset($_POST['artist']) && array_key_exists('artist', $_POST)) {
+      $artist_list = $_POST['artist'];
+      foreach ($artist_list as $artist) {
+        if ($artist != 'select') {
+          $new_song_list = array_merge($new_song_list, retrieveSongsArtist($artist));
+        }
+      }
+    }
+    if (isset($_POST['lang']) && array_key_exists('lang', $_POST)) {
+      $lang_list = $_POST['lang'];
+      foreach ($lang_list as $lang) {
+        if ($lang != 'select') {
+          $new_song_list = array_merge($new_song_list, retrieveSongsLang($lang));
+        }
+      }
+    }
+    if (sizeof($new_song_list) == 0) {
+      echo "<tr><td><h2>No songs available based on your filters</h2></td></tr>";
+    } else {
+      echo '
       <tr>
         <th>Title</th>
         <th>Artist</th>
         <th>Album</th>
-      </tr>
-      <?php
-      include 'database.php';
-      $new_song_list = array();
-      if (isset($_POST['genre']) && array_key_exists('genre', $_POST)) {
-        $genre_list = $_POST['genre'];
-        foreach ($genre_list as $genre) {
-          if ($genre != 'select' ){
-            $new_song_list = array_merge($new_song_list , retrieveSongsGenre($genre));
-          }
-        }
-      }
-      if (isset($_POST['year-max']) && array_key_exists('year-max', $_POST) && isset($_POST['year-min']) && array_key_exists('year-min', $_POST)) {
-        $year_max = $_POST['year-max'];
-        $year_min = $_POST['year-min'];
-        if ($year_min != "" && $year_max != ""){
-          $new_song_list = array_merge($new_song_list , retrieveSongsYear($year_max,$year_min));
-        }
-      }
-      if (isset($_POST['album']) && array_key_exists('album', $_POST)) {
-        $album_list = $_POST['album'];
-        foreach ($album_list as $album) {
-          if ($album != 'select' ){
-            $new_song_list = array_merge($new_song_list , retrieveSongsAlbum($album));
-          }
-        }
-      }
-      if (isset($_POST['artist']) && array_key_exists('artist', $_POST)) {
-        $artist_list = $_POST['artist'];
-        foreach ($artist_list as $artist) {
-          if ($artist != 'select' ){
-            $new_song_list = array_merge($new_song_list , retrieveSongsArtist($artist));
-          }
-        }
-      }
-      if (isset($_POST['lang']) && array_key_exists('lang', $_POST)) {
-        $lang_list = $_POST['lang'];
-        foreach ($lang_list as $lang) {
-          if ($lang != 'select' ){
-            $new_song_list = array_merge($new_song_list , retrieveSongsLang($lang));
-          }
-        }
-      }
+      </tr>';
       while (sizeof($new_song_list) > 20) {
         array_splice($new_song_list, random_int(0, sizeof($new_song_list)), 1);
       }
       shuffle($new_song_list);
-      foreach($new_song_list as $song) {
-        echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() .'</td></tr>';
+      foreach ($new_song_list as $song) {
+        echo '<tr><td>' . $song->getTitle() . '</td><td>' . $song->getArtist() . '</td><td>' . $song->getAlbum() . '</td></tr>';
       }
-      ?>
-    </table>
-    <div class="info-container">
-    <?php if (isset($_SESSION['username']) && $new_song_list != null){
+    }
+    ?>
+  </table>
+  <div class="info-container">
+    <?php if (isset($_SESSION['username']) && $new_song_list != null) {
       $_SESSION['temp_playlist'] = serialize($new_song_list);
-      echo 
+      echo
       '<form action="process-save-playlist.php" method="post">
         <button id="saveButton" type="submit" name="saveNewPlaylist" value="requestSave">Save Playlist</button>
       </form>';
-      }
-      ?>
-    </div>
-  </body>
+    }
+    ?>
+  </div>
+</body>
 
-  <script src="yt-link-script.js"></script>
+<script src="yt-link-script.js"></script>
 
 </html>
-
