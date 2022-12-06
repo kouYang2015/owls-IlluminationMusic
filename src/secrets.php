634b79c994f30140f824e6c3a69cd6305c4df62d
Kou <kou.yang2015@gmail.com>
2022-12-05 23:14:32 -0600

    update db secrets variable

--- src/secrets.php
+++ src/secrets.php
@@ -1,15 +1,13 @@
 <?php
 
 //Metrostate Server Cred - use when running on server
-// $hostname = "localhost";
-// $username = "ics325fa2226";
-// $password = "9427";
-// $database = "ics325fa2226";
+// $db_hostname = "localhost";
+// $db_username = "ics325fa2226";
+// $db_password = "9427";
+// $db_database = "ics325fa2226";
 
 //Local DB cred - use when testing locally
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "illumination_local";
-
-?>
+$db_hostname = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_database = "illumination_local";
